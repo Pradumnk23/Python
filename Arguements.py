63b6cd22340d33d7e1ad26fd9aa09fb96375cec0
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-01-13 10:51:49 +0530

    Add files via upload

---
+++ Arguements.py
@@ -0,0 +1,13 @@
+def get_gender(sex = 'None'):
+    if sex=='m':
+         sex = "male"
+# Here sex ix'm' run but showing syntax error, there should be == sign to show is statement
+    elif sex=='f':
+        sex = "female"
+    else:
+        sex = "none"
+    print(sex)
+get_gender('m')
+get_gender('f')
+get_gender()
+
