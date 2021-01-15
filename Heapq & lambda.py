7d0e0d85d0ac2fa520047b81fb26c840a71b1028
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-01-15 18:45:43 +0530

    Add files via upload

---
+++ Heapq & lambda.py
@@ -0,0 +1,12 @@
+import heapq
+grades = [34, 56, 87, 104, 567, 633]
+print(heapq.nlargest(3, grades))
+
+stocks = [
+    {'ticker': 'AAPL', 'price': 200}
+    {'ticker': 'GOOG', 'price': 700}
+    {'ticker': 'AAPL', 'price': 200}
+    {'ticker': 'AAPL', 'price': 200}
+    {'ticker': 'TUNA', 'price': 58}
+
+]
