e278e423634a4a6dadd8c1eb850701d4fadd8cf1
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-02-25 11:50:07 +0530

    Update Leap year hack.py

--- Leap year hack.py
+++ Leap year hack.py
@@ -1,6 +1,7 @@
 def is_leap(year):
 
-    #return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+    # return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+    # M-2 approach better
     if year % 4 == 0:
         return True
     elif year % 100 == 0:
