55e79d18d9a5b1c86dae49531d71ddd328160e99
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-27 10:47:01 +0530

    Delete Keyword arguments.py

--- Keyword arguments.py
+++
@@ -1,8 +0,0 @@
-def sent(name = 'Pradumn', action = 'ate', item = 'pizza'):
-    print(name, action, item)
-
-sent()
-sent('Chotu', 'eats', 'burger' )
-sent(item='chole')
-sent(name='Puuh')
-# keyword
